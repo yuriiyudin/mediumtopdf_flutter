2dffb508c56da4baf4f376271253b59c41557809
Yurii Yudin <itpodua@gmail.com>
2024-07-08 16:03:29 +0300

    changed build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f0d8219be235a47dc04e75b79e525945
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "5b9aaf2019f2891ada68f2805b31599a",
+"flutter_bootstrap.js": "66fcd36535e3b7c56d87247d2b8d880c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
